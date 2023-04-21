eb841c1fe1f6c56436f6eb2605e0920df5b77c78
Gandalf <zhibu1991@gmail.com>
2023-04-21 14:22:38 +0800

    Update convert-to-diffusion.sh

--- convert-to-diffusion.sh
+++ convert-to-diffusion.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# train_dreambooth.py
 python ../../scripts/convert_diffusers_to_original_stable_diffusion.py \
 --model_path path_to_saved_model/  \
 --checkpoint_path  ppmt.safetensors \
 --use_safetensors
+
+
+# for train_dreambooth_lora.py
+python lora-diffusers-diffusion-convert-to-safetensors.py --file lora_to_saved_model/checkpoint-1000/pytorch_model.bin
