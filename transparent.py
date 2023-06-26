774acdf24af4037485a8a048ea8e57c032c53d88
Gandalf <zhibu1991@gmail.com>
2023-06-26 10:41:04 +0800

    Add files via upload
    Fill the transparent area with white, and fill the other areas outside the transparent area with black, as a mask

---
+++ transparent.py
@@ -0,0 +1,29 @@
+from PIL import Image
+
+
+def transparent_white_png(imgfile, out="white"):
+    img = Image.open(imgfile)
+    img = img.convert("RGBA")
+    W, H = img.size
+    white_pixel = (255, 255, 255, 255)
+    for h in range(W):
+        for w in range(H):
+            if img.getpixel((h, w))[3] == 0:
+                img.putpixel((h, w), white_pixel)
+    img.save(out + ".png")
+
+
+def transparent_black_png(imgfile, out="black"):
+    img = Image.open(imgfile)
+    img = img.convert("RGBA")
+    W, H = img.size
+    black_pixel = (0, 0, 0, 255)
+    for h in range(W):
+        for w in range(H):
+            if img.getpixel((h, w))[3] != 0:
+                img.putpixel((h, w), black_pixel)
+    img.save(out + ".png")
+
+
+transparent_white_png("example.png")
+transparent_black_png("example.png")
