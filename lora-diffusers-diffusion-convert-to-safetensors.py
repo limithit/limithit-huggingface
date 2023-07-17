feb77de35369c3f121f978e1e13e548bb95f56d1
Gandalf <zhibu1991@gmail.com>
2023-07-17 16:14:01 +0800

    Update lora-diffusers-diffusion-convert-to-safetensors.py

--- lora-diffusers-diffusion-convert-to-safetensors.py
+++ lora-diffusers-diffusion-convert-to-safetensors.py
@@ -11,6 +11,15 @@ edit train_dreambooth_lora.py
  531     logging_dir = Path(args.output_dir, args.logging_dir)
  532 
  533     accelerator_project_config = ProjectConfiguration(project_dir=args.output_dir, logging_dir=logging_dir)
+ 534
+ 535     accelerator = Accelerator(
+         gradient_accumulation_steps=args.gradient_accumulation_steps,
+         mixed_precision=args.mixed_precision,
+         log_with=args.report_to,
+         project_config=accelerator_project_config,
+     )
+ 536
+ 537
 
 
 """
